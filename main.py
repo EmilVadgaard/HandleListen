1699a6cc15e45a2b7683c42a307bd9070e9b3682
Emil Nielsen <emiln17@student.sdu.dk>
2023-12-12 01:46:39 +0100

    Initial files and beginning of project

---
+++ main.py
@@ -0,0 +1,20 @@
+from table_manager import *
+3
+running = True
+while running:
+    print('press 1 to add entry to items')
+    print('press 2 to delete entry from items')
+    print('press 3 to show table')
+    print('press any to quit')
+    temp = input()
+    if temp == '1':
+        name = input('What item are you adding?')
+        price = int(input('What does it cost?'))
+        insert_items(name,price)
+    elif temp == '2':
+        name = input('What item do you want to delete?')
+        delete_items(name)
+    elif temp == '3':
+        show_items()
+    else:
+        running = False
---
+++ table_manager.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Djq97hyy',
+    database='shoppinglist'
+    )
+
+mycursor = db.cursor()
+
+#mycursor.execute("CREATE TABLE items (itID int PRIMARY KEY AUTO_INCREMENT, item VARCHAR(255) UNIQUE, price float)")
+
+def insert_items(item: str, price: float) -> None:
+    mycursor.execute("INSERT INTO items (item, price) VALUES (%s,%s)", (item, price))
+    db.commit()
+
+def delete_items(name: str) -> None:
+    #temp = [name]
+    mycursor.execute("DELETE FROM items S WHERE S.item = %s", ([name]))
+    db.commit()
+
+def show_items() -> None:
+    mycursor.execute("SELECT * FROM items")
+    for x in mycursor:
+        print(x)
